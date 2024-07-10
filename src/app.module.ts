39e11148b251ba97ce539fd615f8715c29d5a3d3
Gustavo Mendes <gusm217@gmail.com>
2024-07-10 16:26:00 -0300

    feat: add SerialCommunicationModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SerialCommunicationModule } from './serial-communication/serial-communication.module';
 
 @Module({
-  imports: [],
+  imports: [SerialCommunicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
