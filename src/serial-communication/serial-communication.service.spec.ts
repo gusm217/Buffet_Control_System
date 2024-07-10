483cfc2134eff976569b92062c2df18b1132743c
Gustavo Mendes <gusm217@gmail.com>
2024-07-10 20:04:19 -0300

    test: adjust tests to receive different sets of scales

--- src/serial-communication/serial-communication.service.spec.ts
+++ src/serial-communication/serial-communication.service.spec.ts
@@ -1,19 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SerialCommunicationService } from './serial-communication.service';
 import { SerialPort } from 'serialport';
+import { Scale } from 'src/serial-communication/interfaces/scale.interface';
+import { serialize } from 'v8';
 import { mock } from 'node:test';
 
 jest.mock('serialport');
-const mockOn = jest.fn();
-const mockSerialPort = jest.fn().mockImplementation(() => ({
-  on: mockOn,
-}));
-(SerialPort as jest.MockedClass<typeof SerialPort>).mockImplementation(mockSerialPort);
 
 describe('SerialCommunicationService', () => {
-  let service: SerialCommunicationService;
+	let service: SerialCommunicationService;
+
+	const mockOn = jest.fn();
+	const mockSerialPort = jest.fn().mockImplementation(() => ({
+		on: mockOn,
+	}));
 
   beforeEach(async () => {
+		(SerialPort as jest.MockedClass<typeof SerialPort>).mockImplementation(mockSerialPort);
     const module: TestingModule = await Test.createTestingModule({
       providers: [SerialCommunicationService],
     }).compile();
@@ -25,32 +28,40 @@ describe('SerialCommunicationService', () => {
     expect(service).toBeDefined();
   });
 
-	it('should initialize serial ports', async () => {
-		const mockPorts = [
-			{ path: '/dev/ttyUSB0', manufacturer: 'FTDI' },
-			{ path: '/dev/ttyUSB1', manufacturer: 'FTDI' },
+	it('should initialize scales', async () => {
+		const mockScales: Scale[] = [
+			{ id: 'scale1', portPath: '/dev/ttyUSB0', baudRate: 9600, weighUnit: 'g' },
+			{ id: 'scale2', portPath: '/dev/ttyUSB1', baudRate: 19200, weighUnit: 'kg' },
 		];
 
-		(SerialPort.list as jest.Mock).mockResolvedValue(mockPorts);
+		await service.initializeScales(mockScales);
 
-		const ports = await service.initializePorts();
-		expect(ports).toBeDefined();
-		expect(Array.isArray(ports)).toBe(true);
-		expect(ports.length).toBe(2);
+		expect(mockSerialPort).toHaveBeenCalledTimes(2);
+		expect(mockSerialPort).toHaveBeenCalledWith({ path: '/dev/ttyUSB0', baudRate: 9600 });
+		expect(mockSerialPort).toHaveBeenCalledWith({ path: '/dev/ttyUSB1', baudRate: 19200 });
 	});
 
-	it('should read data from serial port', (done) => {
-		const mockPort = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600});
-		(service as any).ports = [mockPort];
+	it('should read data from a specific scale', async () => {
+		const mockScale: Scale[] = [
+			{ id: 'scale1', portPath: '/dev/ttyUSB0', baudRate: 9600, weighUnit: 'g' },
+		]
 
-		service.onData((data: string) => {
-			expect(data).toBeDefined();
-			expect(typeof data).toBe('string');
-			expect(data).toBe('Test data');
-			done();
-		});
+		await service.initializeScales(mockScale);
+
+		const dataCallback = jest.fn();
+		service.onData('scale1', dataCallback);
+
+		expect(mockOn).toHaveBeenCalledWith('data', expect.any(Function));
+
 
 		const onCallback = mockOn.mock.calls[0][1];
-  	onCallback(Buffer.from('Test data'));
-	})
+		onCallback(Buffer.from('1500'));
+
+		expect(dataCallback).toHaveBeenLastCalledWith(1.5);
+	});
+
+
+	it('should throw an error for non-existent scale', () => {
+		expect(() => service.onData('nonexistent', jest.fn())).toThrow('Scale with id nonexistent not found');
+	});
 });
