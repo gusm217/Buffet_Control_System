a022b913f4d83c0823baef0076f96a8032fbbfde
Gustavo Mendes <gusm217@gmail.com>
2024-07-10 20:04:23 -0300

    feat: adjust tests to receive different sets of scales

--- src/serial-communication/serial-communication.service.ts
+++ src/serial-communication/serial-communication.service.ts
@@ -1,29 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { SerialPort } from 'serialport';
-
-interface PortConfig {
-	path: string;
-	baudRate: number;
-}
+import { Scale } from 'src/serial-communication/interfaces/scale.interface';
 
 @Injectable()
 export class SerialCommunicationService {
-	private ports: SerialPort[] = [];
+	private scales: Map<string, {port: SerialPort; config: Scale }> = new Map();
 
-	async initializePorts(): Promise<SerialPort[]> {
-		const availablePorts = await SerialPort.list();
-		this.ports = availablePorts.map(port => {
-			const config: PortConfig = { path: port.path, baudRate: 9600 };
-			return new SerialPort(config);
-		})
-		return this.ports;
+	async initializeScales(scaleConfigs: Scale[]): Promise<void> {
+		for (const config of scaleConfigs) {
+			const port = new SerialPort({
+				path: config.portPath,
+				baudRate: config.baudRate,
+			});
+			this.scales.set(config.id, { port, config });
+		}
 	}
 
-	onData(callback: (data: string) => void): void {
-		this.ports.forEach(port => {
-			port.on('data', (data:Buffer) => {
-				callback(data.toString());
-			});
-		});
+	onData(scaleId: string, callback: (weight: number) => void): void {
+		const scale = this.scales.get(scaleId);
+		if (!scale) {
+			throw new Error(`Scale with id ${scaleId} not found`);
+		}
+
+		scale.port.on('data', (data: Buffer) => {
+			const weight = this.parseWeight(data, scale.config.weighUnit);
+			callback(weight);
+		})
+	}
+
+	private parseWeight(data: Buffer, unit: 'g' | 'kg'): number {
+		const weightStr = data.toString().trim();
+		const weight = parseFloat(weightStr);
+		return unit === 'kg' ? weight : weight/1000;
 	}
 }
